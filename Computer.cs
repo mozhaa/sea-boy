038ae54e6a6bb421c8addcd547a28d2ea3ff5746
mozhaa <mozhay2005@gmail.com>
2022-10-12 22:12:21 +0300

    Computer может генерировать доску

--- Computer.cs
+++ Computer.cs
@@ -7,22 +7,22 @@ using System.Windows.Controls;
 
 namespace sea_boy
 {
+    public enum Cell
+    {
+        Unknown,
+        Empty,
+        Hit,
+        Kill
+    }
+
+    public enum Outcome
+    {
+        Miss,
+        Hit,
+        Kill
+    }
     internal class Computer
     {
-        private enum Cell
-        {
-            Unknown,
-            Empty,
-            Hit,
-            Kill
-        }
-
-        public enum Outcome
-        {
-            Miss,
-            Hit,
-            Kill
-        }
 
         private Cell[,] board;
         private Random random = new Random();
@@ -59,6 +59,37 @@ namespace sea_boy
                 case Outcome.Kill:
                     board[row, column] = Cell.Kill; break;
             }
+        }
+
+        public BattleShip[,] GenerateBoard()
+        {
+            var newBoard = new BattleShip[Presenter.rows, Presenter.columns];
+            var types = new ShipType[] { ShipType.s1x4, ShipType.s1x3, ShipType.s1x2, ShipType.s1x1 };
+            for (int i = 0; i < 4; i++)
+            {
+                var shipType = types[i];
+                var amount = i + 1;
+                for (int j = 0; j < amount; j++)
+                {
+                    BattleShip battleShip;
+                    int row, column;
+                    while (true)
+                    {
+                        bool orientation = random.Next(3) % 2 == 0;
+                        row = random.Next(Presenter.rows);
+                        column = random.Next(Presenter.columns);
+                        battleShip = new BattleShip(shipType);
+                        if (orientation)
+                            battleShip.Rotate();
+                        if(Presenter.DoNotIntersectAndValidPositionByArray(newBoard, battleShip, row, column))
+                            break;
+                    }
+                    for (int w = row; w < battleShip.Width + row; w++)
+                        for (int h = column; h < battleShip.Height + column; h++)
+                            newBoard[w, h] = battleShip;
+                }
+            }
+            return newBoard;
         }
     }
 }
