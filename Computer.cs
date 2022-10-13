8829b3819acd7458e3e02d10338eed839b9c5707
mozhaa <mozhay2005@gmail.com>
2022-10-14 00:50:41 +0300

    Улучшенный искуственный интеллект

--- Computer.cs
+++ Computer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows;
 
 namespace sea_boy
 {
@@ -33,13 +34,94 @@ namespace sea_boy
 
         public (int, int) AskMove()
         {
+            for (int i = 0; i < Presenter.rows; i++)
+            {
+                for (int j = 0; j < Presenter.columns; j++)
+                {
+                    if (board[i, j] == CellState.Hit)
+                    {
+                        return NextIfHit(i, j);
+                    }
+                }
+            }
+            return RandomMove();
+        }
+
+        private (int, int) RandomMove()
+        {
             var indexes = new List<(int, int)>();
             for (int i = 0; i < Presenter.rows; i++)
                 for (int j = 0; j < Presenter.columns; j++)
-                    if (board[i, j] == CellState.Unknown)
+                    if (board[i, j] == CellState.Unknown && AroundNoShips(i, j))
                         indexes.Add((i, j));
             int index = random.Next(indexes.Count);
             return indexes[index];
+        }
+
+        private bool AroundNoShips(int row, int column)
+        {
+            foreach ((int i, int j) in new List<(int, int)> { (-1, 0), (0, 1), (1, 0), (0, -1) })
+                if (Valid(row + i, column + j) && (board[row + i, column + j] == CellState.Hit || board[row + i, column + j] == CellState.Kill))
+                    return false;
+            return true;
+        }
+
+        private bool Valid(int row, int column)
+        {
+            return 0 <= row && row < Presenter.rows && 0 <= column && column < Presenter.columns;
+        }
+        private (int, int) NextIfHit(int row, int column)
+        {
+            (int, int) unknownCell = (row, column);
+            foreach ((int i, int j) in new List<(int, int)> { (-1, 0), (0, 1), (1, 0), (0, -1) })
+            {
+                if (Valid(row + i, column + j) && board[row + i, column + j] == CellState.Hit)
+                    return NextInLineMove(row, column, i, j);
+                if (Valid(row + i, column + j) && board[row + i, column + j] == CellState.Unknown)
+                    unknownCell = (row + i, column + j);
+            }
+            if (unknownCell == (row, column))
+                return RandomMove();
+            return unknownCell;
+        }
+
+        private (int, int) NextInLineMove(int row, int column, int dx, int dy)
+        {
+            if (dy == 0)
+            {
+                for (int i = row; i < Presenter.rows; i++)
+                {
+                    if (board[i, column] == CellState.Unknown)
+                        return (i, column);
+                    if (board[i, column] == CellState.Empty)
+                        break;
+                }
+                for (int i = row; i >= 0; i--)
+                {
+                    if (board[i, column] == CellState.Unknown)
+                        return (i, column);
+                    if (board[i, column] == CellState.Empty)
+                        break;
+                }
+            }
+            if (dx == 0)
+            {
+                for (int i = column; i < Presenter.columns; i++)
+                {
+                    if (board[row, i] == CellState.Unknown)
+                        return (row, i);
+                    if (board[row, i] == CellState.Empty)
+                        break;
+                }
+                for (int i = column; i >= 0; i--)
+                {
+                    if (board[row, i] == CellState.Unknown)
+                        return (row, i);
+                    if (board[row, i] == CellState.Empty)
+                        break;
+                }
+            }
+            return RandomMove();
         }
 
         public void TellResult(int row, int column, Outcome result, int width=1, int height=1)
